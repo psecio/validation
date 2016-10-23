157fdf335d244d73a08625156a153c37e821af11
Chris Cornutt <enygma@phpdeveloper.org>
2016-10-23 07:57:35 -0500

    more CheckSet tests

--- tests/CheckSetTest.php
+++ tests/CheckSetTest.php
@@ -31,6 +31,12 @@ class CheckSetTest extends \PHPUnit_Framework_TestCase
         $set = new CheckSet($checks);
 
         $this->assertEquals($checks[0], $set[0]);
-    }
+        $this->assertFalse(isset($set[4]));
 
+        // Set a new item and then fetch it
+        $set[2] = new \Psecio\Validation\Check\Boolean();
+        $this->assertInstanceOf('\Psecio\Validation\Check\Boolean', $set[2]);
+        unset($set[2]);
+        $this->assertFalse(isset($set[2]));
+    }
 }
