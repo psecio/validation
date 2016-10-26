f991e6aea26ab0af71c48892accaec4a53a86e2d
Chris Cornutt <enygma@phpdeveloper.org>
2016-10-26 04:50:11 -0500

    adding email check and matching test

---
+++ tests/Check/EmailTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Psecio\Validation\Check;
+
+class EmailTest extends \PHPUnit_Framework_TestCase
+{
+    protected $email;
+
+    public function setUp()
+    {
+        $this->email = new Email();
+    }
+    public function tearDown()
+    {
+        unset($this->email);
+    }
+
+    /**
+     * Test a check of a valid email for a passing result
+     */
+    public function testValidEmail()
+    {
+        $email = 'foo@bar.com';
+        $this->assertTrue($this->email->execute($email));
+    }
+
+    /**
+     * Test a check of an invalid email for a failing result
+     */
+    public function testInvalidEamil()
+    {
+        $email = 'bad-email-address';
+        $this->assertFalse($this->email->execute($email));
+    }
+}
