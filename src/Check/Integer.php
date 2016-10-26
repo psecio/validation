e4b06d33853e273abff016203ff709985945b122
Chris Cornutt <enygma@phpdeveloper.org>
2016-10-26 04:27:56 -0500

    updating integer check to use "min" and "max"

--- src/Check/Integer.php
+++ src/Check/Integer.php
@@ -4,20 +4,23 @@ namespace Psecio\Validation\Check;
 
 class Integer extends \Psecio\Validation\Check
 {
+    public $params = ['min', 'max'];
+
     public function execute($input)
     {
         $fail = false;
         $addl = $this->get();
+
         if (empty($addl)) {
             return is_int($input);
         }
-        if (isset($addl[0])) {
-            if ($input < $addl[0]) {
+        if (isset($addl['min'])) {
+            if ($input < $addl['min']) {
                 $fail = true;
             }
         }
-        if ($fail === false && isset($addl[1])) {
-            if ($input > $addl[1]) {
+        if ($fail === false && isset($addl['max'])) {
+            if ($input > $addl['max']) {
                 $fail = true;
             }
         }
